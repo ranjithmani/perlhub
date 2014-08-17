ae8a370dbced543031902a9c1613d154e3234bc3
ranjithmani <ranjithmani@outlook.com>
2014-08-17 20:33:32 +0530

    A new tcp server script is added

---
+++ TCPserver.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl -W
+use strict;
+use IO::Socket::INET;
+my $port = shift ||  die "[!]no port given\n";
+my $socket = IO::Socket::INET->new(
+	LocalPort	=> $port,
+		Proto		=> 'tcp',
+			Listen 		=> SOMAXCONN ) || die "[!] ca not establish socket..\n";
+			print "Listening for connctions on $port\n";
+			while (my $client = $socket->accept)
+				{
+					my $addr = gethostbyaddr($client->peeraddr, AF_INET);
+						my $port = $client->peerport;
+							while (<$client>)
+									{
+											print "[client:$port] $_";
+													print $client "$.:$_";
+															}
+																close $client || die "[!] connction unable to close....\n";
+																	die "[!]can not connect $!\n";
+																		}
+
