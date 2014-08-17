0031e420f2fdc13fb7ec6a6d32ba002bb1ad1c4d
ranjithmani <ranjithmani@outlook.com>
2014-08-17 22:05:23 +0530

    A program to connect to running TCPserver

---
+++ TCPclient.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -W
+#-----------------------------------------------------------------------------------
+#This program is create TCP client which will connect to a TCPserver 
+#USAGE : TCPclient.pl <server> <port>
+#EXAMPLE : TCPclient.pl localhost 1440(if the server is started @1440 port)
+#----------------------------------------------------------------------------------
+use strict;
+use IO::Socket::INET;
+my $server = shift;
+my $port = shift ;
+my $socket = IO::Socket::INET->new(
+	 	PeerAddr	=> $server,
+		PeerPort	=> $port,
+		Proto		=> 'tcp' ) || die "[!] can not connect to $server\n";
+		print "Establishing connction to $server:$port\n";
+		while (<STDIN>)
+				{
+					print $socket $_;
+					print scalar <$socket>;
+				}
+				close $socket || die "[!] can not close connection\n";
